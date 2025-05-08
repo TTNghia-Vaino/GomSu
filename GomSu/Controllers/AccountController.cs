d512a946e24a58ecfd1b62433f15b5a9e9dfb9b1
Vaino <ttnghia004@gmail.com>
2025-05-08 18:06:17 +0700

    update login

---
+++ GomSu/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GomSu.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
