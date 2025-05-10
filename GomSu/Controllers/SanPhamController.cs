b122cf572ec8d93ac4e2da60bd9004d88cf8a85b
tuan thai <phanthanhthaituan452005@gmail.com>
2025-05-11 03:53:54 +0700

    Revert "gio hang"
    This reverts commit 9e151247f285c453ce10e18a0b1d61ab68e86a7c.

--- GomSu/Controllers/SanPhamController.cs
+++ GomSu/Controllers/SanPhamController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using GomSu.Models;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace GomSu.Controllers
 {
@@ -14,18 +15,12 @@ namespace GomSu.Controllers
             _context = context;
         }
 
-        // GET: SanPham/Index?maLoaiSP={id}
-        public async Task<IActionResult> Index(string maLoaiSP = null)
+        // GET: SanPham
+        public async Task<IActionResult> Index()
         {
-            var sanPhams = string.IsNullOrEmpty(maLoaiSP)
-                ? await _context.SanPhams
-                    .Include(s => s.MaLoaiSpNavigation)
-                    .ToListAsync()
-                : await _context.SanPhams
-                    .Include(s => s.MaLoaiSpNavigation)
-                    .Where(s => s.MaLoaiSp == maLoaiSP)
-                    .ToListAsync();
-
+            var sanPhams = await _context.SanPhams
+                .Include(s => s.MaLoaiSpNavigation)
+                .ToListAsync();
             return View(sanPhams);
         }
 
@@ -50,5 +45,52 @@ namespace GomSu.Controllers
 
             return View(sanPham);
         }
+
+        //// POST: SanPham/AddToCart
+        //[HttpPost]
+        //public async Task<IActionResult> AddToCart(int maSP, int? soLuong)
+        //{
+        //    int maKhachHang = 1; // Replace with actual user ID from authentication
+
+        //    var sanPham = await _context.SanPhams.FindAsync(maSP);
+        //    if (sanPham == null || sanPham.SoLuongTon < (soLuong ?? 1))
+        //    {
+        //        return BadRequest("Sản phẩm không tồn tại hoặc số lượng không đủ.");
+        //    }
+
+        //    var gioHang = await _context.GioHangs
+        //        .FirstOrDefaultAsync(g => g.MaTk == maKhachHang && g.MaSp == maSP);
+
+        //    if (gioHang == null)
+        //    {
+        //        gioHang = new GioHang
+        //        {
+        //            MaKhachHang = maKhachHang,
+        //            MaSP = maSP,
+        //            SoLuong = soLuong ?? 1,
+        //            Gia = (decimal)sanPham.Gia
+        //        };
+        //        _context.GioHangs.Add(gioHang);
+        //    }
+        //    else
+        //    {
+        //        gioHang.SoLuong += (int)(soLuong ?? 1); // Explicit cast to int
+        //        gioHang.Gia = (decimal)sanPham.Gia;
+        //    }
+
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction("Cart");
+        //}
+
+        //// GET: SanPham/Cart
+        //public async Task<IActionResult> Cart()
+        //{
+        //    int maKhachHang = 1; // Replace with actual user ID
+        //    var gioHang = await _context.GioHangs
+        //        .Include(g => g.MaSPNavigation)
+        //        .Where(g => g.MaKhachHang == maKhachHang)
+        //        .ToListAsync();
+        //    return View(gioHang);
+        //}
     }
 }
